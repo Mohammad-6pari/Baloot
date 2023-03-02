283453010c1fe21b32e7c24fc1e6151a30de734a
MohammadrezaSheshpari <msheshpari01@gmail.com>
2023-03-02 12:14:45 +0330

    main added

--- src/main/java/Baloot/Main.java
+++ src/main/java/Baloot/Main.java
@@ -1,6 +1,21 @@
-package org.example;
+package Baloot;
+
+import Baloot.Business.Command;
+import Baloot.Data.Services.IContextManager;
+import Baloot.Data.Services.Implementations.MemoryContextManager;
+import Baloot.Presentation.IView;
+import Baloot.Presentation.Implementations.ConsoleView;
+import Baloot.Presentation.Response;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IView consoleView = new ConsoleView();
+        IContextManager contextManager = new MemoryContextManager();
+
+        while(true) {
+            Command command = consoleView.getInput();
+            Response response = command.execute(contextManager);
+            consoleView.output(response);
+        }
     }
 }
