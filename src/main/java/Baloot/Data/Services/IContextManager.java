4831f8523235f53d407f7f3a6c16975cee234987
MohammadrezaSheshpari <msheshpari01@gmail.com>
2023-03-02 12:27:40 +0330

    services added

---
+++ src/main/java/Baloot/Data/Services/IContextManager.java
@@ -0,0 +1,25 @@
+package Baloot.Data.Services;
+
+import Baloot.Business.DTOs.*;
+import Baloot.Data.Entity.*;
+
+import java.util.List;
+
+public interface IContextManager {
+    User addNewUser(UserDTO userDTO);
+    User getUser(String username);
+    User updateUser(UserDTO userDTO);
+    Provider addProvider(ProviderDTO providerDTO);
+    Provider getProvider(Integer id);
+    Commodity addCommodity(CommodityDTO commodityDTO);
+    Commodity getCommodity(Integer id);
+    CommodityRate getRate(String username, Integer commodityId);
+
+    List<Commodity> getCommoditiesList();
+    Commodity rateCommodity(RateCommodityDTO rateCommodityDTO);
+    Commodity addToBuyList(BuyListItemDTO buyListItemDTO);
+    BuyListItem getBuyListItem(String username, Integer commodityId);
+    BuyListItem removeBuyListItem(BuyListItemDTO buyListItemDTO);
+    List<Commodity> getCommoditiesByCategory(String category);
+    List<Commodity> getBuyListByUsername(String username);
+}
